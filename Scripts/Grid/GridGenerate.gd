036960667d1a46a515f294c45623096ba9786a0a
Red Dude <isaaconeoneone@gmail.com>
2024-01-14 01:07:49 +0300

    Mouse Following Implementation. Viewport input included

--- Scripts/Grid/GridGenerate.gd
+++ Scripts/Grid/GridGenerate.gd
@@ -7,8 +7,8 @@ var GridPoint = preload("res://Prefabs/Grid/grid_p_oint.tscn")
 @onready var GridParent = $"./GridHolder"
 
 func _ready() -> void:
-	for x in range(-5,5):
-		for z in range(-5,5):
+	for x in range(-5,6):
+		for z in range(-5,6):
 			var GridIns = GridPoint.instantiate()
 			GridIns.set_position(Vector3(x,0,z))
 			GridParent.add_child(GridIns)
--- Scripts/Tools/BoxTool.gd
+++ Scripts/Tools/BoxTool.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
-@export var Camera : Node3D
+
+@export var Camera : Camera3D
+@export var ViewPortF : SubViewport
+
 @export var GridManaged : Node3D
+
 @export var DebugPoint : Node3D
+@export var DebugDirection:Node3D
 
 var GridY : float
 
 var ViewWidth : int
 var ViewHeigh : int
-
 # vector math
 var PointDistance
 
@@ -16,24 +20,25 @@ var PointDistance
 func _ready():
 	GridY = GridManaged.GridY
 
-func _process(_delta):
-	PointDistance = abs(tan(Camera.global_rotation.x- PI/2))
-	
-	var mouse_position =get_viewport().get_mouse_position()
-	
-	var CameraY = Camera.global_position.y
-	var Direction = -Camera.global_basis.z
 
+func _process(_delta):
+	
+	var mouse_position = ViewPortF.get_mouse_position()
+	var MouseDir = Camera.project_ray_normal(mouse_position)
+	#Direction of PseudoRay from 0 by Y and X, Z of Camera
+	var CameraY = Camera.global_position.y
+	var Direction = MouseDir
 	Direction.y = 0
 	Direction = Direction.normalized()
-	
+	#Distance to Grid of PseudoRay
+	#Get the Rotation needed to look in a vector of mouse Ray and gets tg 
+	#of it. might be complicated to understand.
+	PointDistance = abs(tan(transform.looking_at(MouseDir, Vector3.UP).basis.get_euler().x -PI/2))
 	#DO NOT CHANGE | СУКА ЛОМАЕТСЯ ТОКА ПОВОД ДАЙ :: В случае чего хотябы закоментировать
 	DebugPoint.transform.origin = Camera.global_position + Direction * PointDistance * abs(CameraY)
 	DebugPoint.transform.origin.y = GridY
+	#DebugPoint.global_rotation = MouseDir+Camera.global_rotation
+	DebugDirection.rotation = transform.looking_at(DebugPoint.position, Vector3.UP).basis.get_euler()
 	
-	
-	#print(Direction)
-	print("Point is")
-	print(PointDistance)
-	print("Camera is(Rot)")
-	print(Camera.global_rotation_degrees.x+90)
+	print(mouse_position)
+
