70f482aba4ca7ade6ffe2a5cf74a578b1b283400
Red Dude <isaaconeoneone@gmail.com>
2024-03-07 19:49:56 +0300

    1.0.5.2
    я хуй знает зачем ну на всякий

--- Scripts/Tools/Do-Undo.gd
+++ Scripts/Tools/Do-Undo.gd
@@ -1,11 +1,8 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func add_command():
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func undo_command():
 	pass
